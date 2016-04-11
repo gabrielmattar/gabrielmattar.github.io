0885caee2c23605c0b48292f4cb22f7d55f65588
Gabriel Mattar <gabrielmattar41@gmail.com>
2016-04-11 11:46:00 -0300

    Tweak no script do menu login, agora ele desaparece quando ha um click fora dele

--- js/login.js
+++ js/login.js
@@ -1,11 +1,24 @@
 var login = document.getElementById('login');
 var campologin = document.getElementsByClassName('login');
 
-login.addEventListener('click', function(e) {
+document.addEventListener('click', desaparece, false);
+login.addEventListener('click', aparece, false);
+
+function aparece(e) {
     if(window.innerWidth > 860)
       campologin[0].classList.toggle('showup1');
     else
       campologin[0].classList.toggle('showup2');
 
-    
-}, false);
+}
+
+function desaparece(e){
+  if(e.target != login){
+    if(campologin[0].classList.contains('showup1')){
+        campologin[0].classList.toggle('showup1');
+    }
+    if(campologin[0].classList.contains('showup2')){
+        campologin[0].classList.toggle('showup2');
+    }
+  }
+}
